59597ecc93bcb7b5f3717f2e29a0b3ea5d25957e
umair140404 <23100053@lums.edu.pk>
2024-10-21 16:23:37 +0500

    ds course code, updated description

--- src/data/teachingExperience.js
+++ src/data/teachingExperience.js
@@ -1,10 +1,11 @@
 const teachingExperience = [
   {
     year: "Spring 2023",
-    title: "TA for the course Data Structures",
+    title: "TA for the course Data Structures (CS 202)",
     company: "Lahore University of Management Sciences",
     href: "https://lums.edu.pk",
     description: [
+      "Managed a class of 100+ students alongside my Co-TAs",
       "Created and checked assignments & quizzes",
       "Held tutorials & office hours",
     ],
